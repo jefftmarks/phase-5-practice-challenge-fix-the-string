d90c5de50ed9db6b5b08b6bfe02b7f920fcfecfc
Jeff Marks <jeff.t.marks@gmail.com>
2022-11-14 11:42:36 -0500

    done with assignment

--- lib/fix_the_string.rb
+++ lib/fix_the_string.rb
@@ -1,1 +1,34 @@
-# your code here
+require 'pry'
+
+# expect(fix_the_string('cCcOdDdInGGgisSSfUUun')).to eq('cOdInGiSfUn')
+# expect(fix_the_string('efFEFiiIxyeEYeZdDzd')).to eq('Fixed')
+
+def isOppositeCase(char1, char2)
+	if char1 != char2 && (char1.upcase == char2 || char2.upcase == char1)
+		return true
+	end
+	return false
+end
+
+def fix_the_string(str)
+	stack = []
+	queue = str.split("")
+	while queue.length > 0
+		char = queue.shift
+		if isOppositeCase(char, stack[stack.length - 1] || "")
+			stack.pop
+		else
+			stack.push(char)
+		end
+	end
+
+	stack.join()
+end
+
+=begin
+Initialize empty stack array
+Shift first letter from string
+If last character in stack is same letter and opposite case:
+	Remove last character from stack
+Else, add it to end of stack
+=end
